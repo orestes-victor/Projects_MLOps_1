ee71dd92821f25af3913847e84d904631bb12358
Victor <orestesvictor@gmail.com>
2024-05-26 20:59:08 -0300

    Actualización

--- 02_pipelines.py
+++ 02_pipelines.py
@@ -5,31 +5,34 @@ def remove_negative_values(df, column):
     df[column] = df[column].apply(lambda x: np.nan if x < 0 else x)
     return df
 
-def remove_ouliers_with_zscore(df, column, threshold=2):
-    column_mean = df[column].mean()
+def remove_ouliers_with_zscore(df, column, threshold = 2):
+    column_mean =  df[column].mean()
     column_std = df[column].std()
     df[column] = df[column].mask(((df[column] - column_mean) / column_std).abs() > threshold, column_mean)
-    return df
+    return df 
 
 def map_column_values(df, column, mapping_dict):
     df[column] = df[column].apply(lambda value: mapping_dict.get(value.lower().strip(), np.nan) if value is not np.nan else np.nan)
     return df
 
 def fill_na_in_column(df, column, fill_value):
-    df[column] = df[column].fillna(fill_value)
+    df[column].fillna(fill_value, inplace=True)
     return df
 
 def preprocess_data(df):
+
     education_mapping = {
         "Bachelors": "Bachelor",
         "mastre": "Master",
         "pHd": "PhD",
         "no education": "None"
     }
+
     gender_mapping = {
         "m" : "M",
         "f" : "F"
     }
+
     return (
         df.pipe(remove_negative_values, "Edad")
         .pipe(remove_negative_values, "Ingresos")
@@ -48,6 +51,7 @@ def preprocess_data(df):
         .pipe(fill_na_in_column, "Ingresos", df["Ingresos"].mean())
         .pipe(fill_na_in_column, "Edad", df["Edad"].mean())
     )
+
 
 df = pd.read_csv("tratamiento_datos.csv", index_col=0)
 print(df)
